be7af58581426f2a06f9584f35d662551f4ac907
iamshakur <salmanshakur7@gmail.com>
2023-10-31 14:59:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const $i = document.querySelector("i");
+let progress = 0;
+
+setInterval(() => {
+	progress++;
+	$i.innerText = `${progress}%`;
+	if (progress === 100) {
+		progress = 0;
+	}
+	handler: 100;
+});
